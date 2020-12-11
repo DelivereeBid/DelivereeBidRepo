c67917080b9aaaf239623eff8f65581eb32f4071
delviawp <delviawp@gmail.com>
2020-12-11 14:43:54 +0800

    register and login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import  {LoginPage}  from '../components'
 
 function Login (props) {
 
     return (
         <>
             <h1>ini login</h1>
+            <LoginPage/>
         </>
     )
 }
