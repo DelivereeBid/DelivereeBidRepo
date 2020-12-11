c11382a3a30ff6b2fba6b428b94aa1fce983c705
mrzacq <hartantoaanan@gmail.com>
2020-12-11 19:23:16 +0700

    setup unggah done and wait aws key

--- server/router/service.js
+++ server/router/service.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const ServiceController = require('../controllers/service')
-
+const upload = require('../middlewares/upload')
 const {authenticationTransporter, authorizationService} = require('../middlewares/auth')
 
 router.use(authenticationTransporter)
 router.get("/", ServiceController.findAll)
 router.get("/:id", ServiceController.getById)
-router.post("/", ServiceController.createService)
-router.put("/:id", authorizationService, ServiceController.updateService)
+router.post("/", upload.single('file'), ServiceController.createService)
+router.put("/:id", authorizationService, upload.single('file'), ServiceController.updateService)
 router.delete("/:id", authorizationService, ServiceController.deleteService)
 
 
