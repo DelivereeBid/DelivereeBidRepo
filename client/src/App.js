d9bb4a9e4a8c62f0397a5f4121b1825df0196a3c
RafiP <rafi.panji.p@gmail.com>
2020-12-11 11:06:43 +0700

    switch and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
-// import { Home, Category, Favorite } from './pages'
+import { HomeShipper, HomeTransporter, CreatePostShipper, PaymentMethod, DetailPostShipper, Wallet, ComplaintPage, ControlPage, Login, RatingPage, Register, VechileInformation } from './pages'
 import { Provider} from 'react-redux'
 import store from './store'
 
@@ -10,6 +10,48 @@ function App() {
       <nav>
 
       </nav>
+
+      <Switch>
+          <Route exact path='/shipper'>
+            <HomeShipper/>
+          </Route>
+          <Route exact path='/shipper/createPost'>
+            <CreatePostShipper/>
+          </Route>
+          <Route exact path='/shipper/payment'>
+            <PaymentMethod/>
+          </Route>
+          <Route exact path='/transporter'>
+            <HomeTransporter/>
+          </Route>
+          <Route exact path='/transporter'>
+            <HomeTransporter/>
+          </Route>
+          <Route exact path='/transporter/detailPost'>
+            <DetailPostShipper/>
+          </Route>
+          <Route exact path='/transporter/wallet'>
+            <Wallet/>
+          </Route>
+          <Route path='/complaint'>
+            <ComplaintPage/>
+          </Route>
+          <Route path='/controlPage'>
+            <ControlPage/>
+          </Route>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+          <Route path='/register'>
+            <Register/>
+          </Route>
+          <Route path='/ratingPage'>
+            <RatingPage/>
+          </Route>
+          <Route path='/vechileInformation'>
+            <VechileInformation/>
+          </Route>
+        </Switch>
     </Provider>
   );
 }
