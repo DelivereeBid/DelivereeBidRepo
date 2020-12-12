e428c4ae6bb5f123c70751da211ed1d20f526aa1
RamaRamesses <maulanaribrahim@outlook.com>
2020-12-12 15:13:13 +0700

    Temporary upload attempt

--- server/app.js
+++ server/app.js
@@ -4,12 +4,30 @@ const port = process.env.PORT || 3000
 const cors = require('cors')
 const router = require('./router')
 const err = require('./middlewares/errHandler')
+const upload = require('./middlewares/upload');
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use(router)
+app.post('/upload-single', upload.single('file'), (req, res) => {
+    console.log(req.body)
+    res.end()
+  })
+
+app.post('/upload-array', upload.array('files'), (req, res) => {
+console.log(req.body)
+res.end()
+})
+
+app.post('/upload-fields',
+upload.fields([{ name: 'file1' }, { name: 'file2' }]),
+(req, res) => {
+    console.log(req.body)
+    res.end()
+}
+)
 app.use(err)
 
 // app.listen(port, () => console.log(`server running: http://localhost:${port}`))
