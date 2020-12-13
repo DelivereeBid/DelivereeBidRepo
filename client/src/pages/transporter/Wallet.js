386c68a00d074a2d546979b07c2032bfb250eabe
mrzacq <hartantoaanan@gmail.com>
2020-12-13 20:39:16 +0700

    add transporter wallet

--- client/src/pages/transporter/Wallet.js
+++ client/src/pages/transporter/Wallet.js
@@ -1,12 +1,62 @@
-import React from 'react'
+import React from "react";
+import { Container, Card, Button, Col, Row, Image } from "react-bootstrap";
 
-function Wallet (props) {
-
-    return (
-        <>
-            <h1>ini Wallet</h1>
-        </>
-    )
+function Wallet(props) {
+  return (
+    <>
+      <Container style={{ marginTop: 100 }}>
+        <Row>
+          <Col xs={6} md={6}>
+            <Card
+              style={{
+                border: "primary",
+                borderStyle: "rounded",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Card.Body>
+                <Card.Title
+                  style={{
+                    textAlign: "center",
+                    marginTop: 20,
+                    marginBottom: 20,
+                  }}
+                >
+                  Ini wallet
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={6} md={6}>
+            <Card
+              style={{
+                border: "primary",
+                borderStyle: "rounded",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Card.Body>
+                <Card.Title
+                  style={{
+                    textAlign: "center",
+                    marginTop: 20,
+                    marginBottom: 20,
+                  }}
+                >
+                  Ini Pembayaran
+                </Card.Title>
+                <Card.Footer>
+                <Button style={{marginLeft: 180}} variant="primary">Checkout</Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Wallet
+export default Wallet;
