6e55ec8d46bd85d07f5052511a56d753eba92260
mrzacq <hartantoaanan@gmail.com>
2020-12-14 19:25:19 +0700

    filter bid that create by shipper only

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -10,16 +10,15 @@ function LoginPage (props) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const access_token = useSelector(state => state.access_token)
   const handleLogin = (e) => {
-    console.log(access_token, 'dklsalkj')
     e.preventDefault()
     const payload = {
       email,password
     }
     dispatch(setLogin(payload))
-    // history.push('/transporter')
+    history.push('/transporter')
   }
+  
 
   const handleRegister = () => {
     history.push('/transporter-register')
