1a427628e01d06c74c22c64250981270facc99ed
RamaRamesses <maulanaribrahim@outlook.com>
2020-12-16 01:13:34 +0700

    Fixed styling conflict

--- client/src/pages/LoginShipper.js
+++ client/src/pages/LoginShipper.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { setLoginShipper } from "../store";
 import { Container, Form, Button } from "react-bootstrap";
+import './Login.css'
 
 function LoginShipper(props) {
   const history = useHistory();
@@ -44,7 +45,7 @@ function LoginShipper(props) {
                 </div>
                 <div class="col-lg-12 login-form">
                     <div class="col-lg-12 login-form">
-                        <form onSubmit={(e) => handleLogin(e)}>
+                        <form className='inputLogin' onSubmit={(e) => handleLogin(e)}>
                             <div class="form-group">
                                 <label class="form-control-label">EMAIL</label>
                                 <input onChange={(e) => setEmail(e.target.value)} type="email" class="form-control"></input>
@@ -59,12 +60,12 @@ function LoginShipper(props) {
                                 <p role="button" onClick={() => handleRegister()}>Don't have an account?</p>   
                                 </div>
                             </div>
+                            <div class="col-lg-3 col-md-2">
+                            <button type="submit" class="btn btn-outline-primary">Sign In</button>
+                            <button onClick={() => toHome()} type="submit" class="btn btn-outline-primary mb-5">Back</button>
+                            </div>
                         </form>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-2">
-                <button type="submit" class="btn btn-outline-primary">Sign In</button>
-                <button onClick={() => toHome()} type="submit" class="btn btn-outline-primary mb-5">Back</button>
                 </div>
             </div>
         </div>
