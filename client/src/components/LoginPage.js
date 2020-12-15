4c3dc5ca6dd1d4b869700a394e5baf27198af588
Delviawp <delviawp@gmail.com>
2020-12-16 00:48:49 +0800

    new landing and login page

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -32,43 +32,43 @@ function LoginPage(props) {
   }
   return (
     <>
-    <Container>
-        <Form onSubmit={(e) => handleLogin(e)}>
-          <Form.Group>
-            <h2 className="mb-3 mt-5">Sign In as Transporter</h2>
+     <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 login-box">
+                <div class="col-lg-12 login-key">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">
+                    Sign In As Transporter
+                </div>
+                <div class="col-lg-12 login-form">
+                    <div class="col-lg-12 login-form">
+                        <form onSubmit={(e) => handleLogin(e)}>
+                            <div class="form-group">
+                                <label class="form-control-label">EMAIL</label>
+                                <input onChange={(e) => setEmail(e.target.value)} type="email" class="form-control"></input>
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label">PASSWORD</label>
+                                <input onChange={(e) => setPassword(e.target.value)} type="password" class="form-control" i></input>
+                            </div>
 
-            <Form.Group controlId="form.email">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                placeholder="Enter email"
-                name="email"
-              />
-              <Form.Text as="div" className="text text-danger"></Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="form.password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Form.Text as="div" className="text text-danger"></Form.Text>
-            </Form.Group>
-
-            <Form.Group>
-            <Button variant="outline-primary" type="submit">
-              Sign in
-            </Button>
-            <Button variant="outline-success" onClick={() => handleRegister()}>Create Transporter account?</Button>
-            <Button variant="outline-danger" onClick={() => toHome()}>Cancel</Button>
-            </Form.Group>
-          </Form.Group>
-        </Form>
-        </Container>
+                            <div class="col-lg-12 loginbttm">
+                                <div class="col-lg-6 login-btm login-text">
+                                <p role="button" onClick={() => handleRegister()}>Don't have an account?</p>   
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-2">
+                <button type="submit" class="btn btn-outline-primary">Sign In</button>
+                <button onClick={() => toHome()} type="submit" class="btn btn-outline-primary mb-5">Back</button>
+                </div>
+            </div>
+        </div>
+        </div>
     </>
   );
 }
